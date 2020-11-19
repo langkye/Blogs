6b4d6dde9fa3fa81e2e7a223d504b9ae16604432
langkye <langkye@gmail.com>
2020-11-20 02:51:02 +0800

    init blogs-java

---
+++ blogs-java/src/main/java/com/langkye/Application.java
@@ -0,0 +1,16 @@
+package com.langkye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author langkye
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
